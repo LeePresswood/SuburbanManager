9e4c32743d68c7e3fd8868296f53a29fa02406f3
Lee Presswood <pressedlee123@gmail.com>
2015-03-08 03:47:16 -0500

    Made project.

---
+++ app/desktop/src/com/leepresswood/suburbanmanager/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.leepresswood.suburbanmanager.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.leepresswood.suburbanmanager.GameSM;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameSM(), config);
+	}
+}
